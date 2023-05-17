f7c093e650604a80495906f68eef0a7a09fb0d51
caodudu <caojundudu@qq.com>
2023-05-17 22:12:34 +0800

    modified

--- cyber/use/report_project.py
+++ cyber/use/report_project.py
@@ -3,10 +3,10 @@ import argparse
 
 
 def start_cyber(project_path):
-    if not os.path.exists(path):
-        os.makedirs(path)
+    if not os.path.exists(project_path):
+        os.makedirs(project_path)
 
-    file_path = os.path.join(path, 'cyber.yaml')
+    file_path = os.path.join(project_path, 'cyber.yaml')
     if not os.path.exists(file_path):
         with open(file_path, 'w') as f:
             f.write('Setting: \n  Location: \n  Laboratory: \n  Operator: \n  Platform: \n\nConfig: \n  input: \n  script: \n  output: \n  report: \n  path_wkthmltopdf: \n  fastchat:')
