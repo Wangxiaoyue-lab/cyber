9f61eeea182d9804d33ffecd9cc85ec1f95cf874
caodudu <caojundudu@qq.com>
2023-05-19 19:21:16 +0800

    modified

--- cyber_py/alias.py
+++ cyber_py/alias.py
@@ -3,7 +3,7 @@ import os
 absolute_path = os.path.split(os.path.realpath(__file__))[0]
 cyber_path = os.path.split(absolute_path)[0]
 alias_cmd_dir = f"alias cyber_dir='(cd {cyber_path} && pwd)'"
-alias_cmd_pacakages = f'cyber_pacakages() {{ cd "{cyber_path}" && python -m cyber_py.support._4_cyber_shell "$@"; cd -; }}'
+alias_cmd_pacakages = f'cyber_packages() {{ cd "{cyber_path}" && python -m cyber_py.support._4_cyber_shell "$@"; cd -; }}'
 alias_cmd_p = f'cyber_p() {{ cd "{cyber_path}" && python -m cyber_py.report_project "$@"; cd -; }}'
 alias_cmd_f = f'cyber_f() {{ cd "{cyber_path}" && python -m cyber_py.formalize "$@"; cd -; }}'
 alias_cmd_r = f'cyber_r() {{ cd "{cyber_path}" && python -m cyber_py.report_task "$@"; cd -; }}'
