a5da98e5a2ecae04b14a908d760bb95d47e1e2c6
caodudu <caojundudu@qq.com>
2023-05-19 18:18:48 +0800

    modified

--- cyber_py/alias.py
+++ cyber_py/alias.py
@@ -3,15 +3,19 @@ import os
 absolute_path = os.path.split(os.path.realpath(__file__))[0]
 cyber_path = os.path.split(absolute_path)[0]
 alias_cmd_dir = f"alias cyber_dir='(cd {cyber_path} && pwd)'"
-alias_cmd_pacakages = f"alias cyber_pacakages='cd {cyber_path} && python -m cyber_py.support._4_cyber_shell $@ && popd'"
-alias_cmd_p = f"alias cyber_p='pushd {cyber_path} && python -m cyber_py.report_project $@ && popd'"
-alias_cmd_f = f"alias cyber_f='pushd {cyber_path} && python -m cyber_py.formalize $@ && popd'"
-alias_cmd_r = f"alias cyber_r='pushd {cyber_path} && python -m cyber_py.report_task $@ && popd'"
-with open(os.path.expanduser("~/.bashrc"),"a") as f:
+alias_cmd_pacakages = f'cyber_pacakages() {{ cd "{cyber_path}" && python -m cyber_py.support._4_cyber_shell "$@"; cd -; }}'
+alias_cmd_p = f'cyber_p() {{ cd "{cyber_path}" && python -m cyber_py.report_project "$@"; cd -; }}'
+alias_cmd_f = f'cyber_f() {{ cd "{cyber_path}" && python -m cyber_py.formalize "$@"; cd -; }}'
+alias_cmd_r = f'cyber_r() {{ cd "{cyber_path}" && python -m cyber_py.report_task "$@"; cd -; }}'
+ 
+with open(os.path.expanduser("~/.cyberrc"),"a") as f:
     f.write(alias_cmd_dir + "\n")
     f.write(alias_cmd_pacakages + "\n")
     f.write(alias_cmd_p + "\n")
     f.write(alias_cmd_f + "\n")
     f.write(alias_cmd_r + "\n")
+
+with open(os.path.expanduser("~/.bashrc"),"a") as f:
+    f.write('if [ -f ~/.cyberrc ]; then source ~/.cyberrc fi\n')
 
 os.system("source ~/.bashrc")
